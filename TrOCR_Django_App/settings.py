60aeb1e5f624a580b6894d73df0f4c82b6651a02
Abdallah WallyAllah <abdallah.wallyallah97@gmail.com>
2023-09-14 00:00:50 +0300

    Changes path to be absolute

--- TrOCR_Django_App/settings.py
+++ TrOCR_Django_App/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'TrOCR_Django_App.urls'
 
+templates_dir = os.path.join(os.getcwd(), "TrOCR_Django_App","templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [templates_dir],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

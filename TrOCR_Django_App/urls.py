83f0094418ba8c2fa268302b99252694e08e34ee
Abdallah WallyAllah <abdallah.wallyallah97@gmail.com>
2023-09-13 23:15:21 +0300

    Adding main HTML template

--- TrOCR_Django_App/urls.py
+++ TrOCR_Django_App/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.ocr_task_view, name='ocr_task_view'),
+    path('Deep_Learning_APP/', include('Deep_Learning_APP.urls')),
     path('admin/', admin.site.urls),
 ]
